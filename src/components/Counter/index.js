7e3412e4133d691598bcd3d27138c992dec4459a
Kévin Giraud <kevin.giraud@cdbdx.biz>
2023-06-06 10:38:00 +0200

    feat : update Counter with react-use

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,18 +1,13 @@
-import { useState } from "react";
+import { useCounter } from "react-use";
 
 export default function Counter({ startValue = 0, step = 1 }) {
-  const [currentValue, setNextValue] = useState(startValue);
+  const [currentValue, { dec, inc }] = useCounter(startValue, null, startValue);
 
   return (
     <>
       <p>{currentValue}</p>
-      <button
-        onClick={() => {
-          setNextValue(currentValue + step);
-        }}
-      >
-        Click Me
-      </button>
+      <button onClick={() => dec(step)}>-{step}</button>
+      <button onClick={() => inc(step)}>+{step}</button>
     </>
   );
 }
