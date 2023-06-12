41426e80caaf0944b26d885741d675843a210e8a
Kévin Giraud <kevin.giraud@cdbdx.biz>
2023-06-12 16:10:04 +0200

    feat: 🎸 Seller App redirects to HomePage

--- src/ds/molecules/Header/index.js
+++ src/ds/molecules/Header/index.js
@@ -1,13 +1,20 @@
 import { string } from "prop-types";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Link as MUILink, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Header({ title, caption }) {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h5" component="h1">
-          {title}
-        </Typography>
+        <MUILink
+          component={Link}
+          to="/"
+          sx={{ color: "white", textDecoration: "none" }}
+        >
+          <Typography variant="h5" component="h1">
+            {title}
+          </Typography>
+        </MUILink>
         <Typography
           sx={{
             paddingLeft: 2,
