17900bf78b5558a409a04ad4448bbfe7ef82d738
Perrine Blais <perrine.blais@cdbdx.biz>
2023-06-13 14:58:57 +0200

    test: 💍 Improve UTs on Counter

--- src/components/Counter/index.test.js
+++ src/components/Counter/index.test.js
@@ -18,8 +18,10 @@ test("that true is true", () => {
 });
 
 const counterValue = () => screen.getByRole("marquee");
-const incButton = () => screen.getByRole("button", { name: "+1" });
-const descButton = () => screen.getByRole("button", { name: "-1" });
+const incButton = ({ step = 1 } = {}) =>
+  screen.getByRole("button", { name: `+${step}` });
+const decButton = ({ step = 1 } = {}) =>
+  screen.getByRole("button", { name: `-${step}` });
 
 describe("Counter", () => {
   it("renders the counter value and the 2 dec/inc buttons", () => {
@@ -27,7 +29,7 @@ describe("Counter", () => {
 
     expect(counterValue()).toHaveTextContent("0");
     expect(incButton()).toBeInTheDocument();
-    expect(descButton()).toBeInTheDocument();
+    expect(decButton()).toBeInTheDocument();
   });
 
   it("increments/decrements properly the counter value", () => {
@@ -36,7 +38,39 @@ describe("Counter", () => {
     userEvent.click(incButton());
     expect(counterValue()).toHaveTextContent("0");
 
-    userEvent.click(descButton());
+    userEvent.click(decButton());
     expect(counterValue()).toHaveTextContent("0");
+  });
+
+  describe("while startValue === 10", () => {
+    it("renders the counter value and the 2 dec/inc buttons", () => {
+      render(<Counter startValue={10} />);
+
+      expect(counterValue()).toHaveTextContent(/^10$/);
+      expect(incButton()).toBeInTheDocument();
+      expect(decButton()).toBeInTheDocument();
+    });
+
+    it("increments and decrements properly the counter value", () => {
+      render(<Counter startValue={10} />);
+
+      userEvent.click(incButton());
+      expect(counterValue()).toHaveTextContent(/^10$/);
+
+      userEvent.click(decButton());
+      expect(counterValue()).toHaveTextContent(/^10$/);
+    });
+  });
+
+  describe("while step = 3", () => {
+    const step = 3;
+
+    it("renders the counter value and the 2 dec/inc buttons", () => {
+      render(<Counter step={3} />);
+
+      expect(counterValue()).toHaveTextContent("0");
+      expect(incButton({ step })).toBeInTheDocument();
+      expect(decButton({ step })).toBeInTheDocument();
+    });
   });
 });
